271806f6e44677fc3da9c2a43c5fc0d9a00db79c
Sobirov S <sobiroff23@gmail.com>
2024-12-05 10:21:49 +0500

    fixed

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <template>
 <div class="flex items-center justify-center">
   <div class="mt-[64px] h-[88px] w-[80%] bg-[#6E5DC6] rounded-xl">
-    
+        <img class="py-6 pl-[16px]" src="../public/icon puck.svg" alt="">
   </div>
 </div>
 </template>
