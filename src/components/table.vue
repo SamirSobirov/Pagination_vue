281ff80ffd88820276b6547bca35e600146f3e55
Sobirov S <sobiroff23@gmail.com>
2024-12-05 16:06:55 +0500

    foxed

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div class="flex flex-col">
     <!-- 1 -->
@@ -127,39 +125,45 @@
     </div>
     <hr class="pb-[40px]" />
 
-
     <!-- 6 -->
     <div class="flex w-[1100px] justify-between items-center p-4">
-      
+      <button
+        class="flex border items-center gap-4 border-[#475569] rounded-3xl px-4 py-2"
+      >
+        <p class="text-[#475569]">Показать по 5</p>
+        <img src="../../public/icons/Arrow-Down2.svg" alt="" />
+      </button>
 
-        <button class="flex border items-center gap-4 border-[#475569] rounded-3xl px-4 py-2">
-            <p class="text-[#475569]">Показать по 5</p>
-            <img src="../../public/icons/Arrow-Down2.svg" alt="">
+    
+    
+    
+      <div class="flex gap-4 items-center justify-center text-center">
+        <button>
+            <img src="../../public/icons/ArrowLeft-Outline.svg" alt="">
         </button>
-        
-        
-      
-
-
-      <div class="flex">
-        <p>Авг 5,2022</p>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>
+            <img class="rotate-180" src="../../public/icons/ArrowLeft-Outline.svg" alt="">
+        </button>
       </div>
     </div>
 
 
 
-    <!-- Повторите для остальных секций -->
   </div>
 
-
-
-
-  <!-- 2 -->
 </template>
 
 <script>
 export default {
   name: "tableVue",
 };
-
 </script>
