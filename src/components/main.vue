ac04bbd8e48dce1d45e624866eda49f4702ebd08
Sobirov S <sobiroff23@gmail.com>
2024-12-05 11:32:45 +0500

    fixed

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,20 +1,120 @@
 <template>
-    <div class="flex items-center justify-center">
-        <div class="mt-[64px] h-[500px] w-[80%] bg-[#d0c7ff] rounded-xl flex">
+  <div class="flex items-center flex-c justify-center">
+    <div class="mt-[64px] h-[500px] w-[80%] bg-[#d0c7ff] rounded-xl flex flex-col">
+      <div class="w-[250px] h-[500px] bg-slate-500 flex flex-col gap-2">
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Заказы</p>
+        </button>
 
 
-        </div>
-      
-        
-      
-      
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Договора</p>
+        </button>
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Сообщество</p>
+        </button>
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Уведомления</p>
+        </button>
+
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Заявки</p>
+        </button>
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Избранные</p>
+        </button>
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Отчеты</p>
+        </button>
+
+
+
+        <button
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+        >
+          <img
+            class="w-[20px] h-[20px]"
+            src="../../public/icons/shop.svg"
+            alt=""
+          />
+          <p class="text-lg text-[#475569] font-semibold">Настройки</p>
+        </button>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "mainVue",
-  };
-  </script>
-  
-  <style scoped></style>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mainVue",
+};
+</script>
+
+<style scoped></style>
