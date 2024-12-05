a74824aa37b956bf914169fbcf9cf85500e31795
Sobirov S <sobiroff23@gmail.com>
2024-12-05 11:59:20 +0500

    fixe

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,110 +1,111 @@
 <template>
   <div class="flex items-center flex-c justify-center">
     <div class="mt-[64px] h-[500px] w-[80%] bg-[#d0c7ff] flex rounded-3xl">
-      <div class="w-[250px] h-[500px] bg-slate-500 flex flex-col gap-2 ">
+      <div class="mt-[10px] w-[250px] h-[100%] flex flex-col gap-2 ml-[3px]">
         <button
-          class="mt-[10px] flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
           <img
-            class="w-[20px] h-[20px]"
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/shop.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Заказы</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >
+            Заказы
+          </p>
         </button>
 
-
-
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
           <img
-            class="w-[20px] h-[20px]"
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/agreement.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Договора</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Договора</p>
         </button>
 
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
           <img
-            class="w-[20px] h-[20px]"
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/Community.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Сообщество</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Сообщество</p>
         </button>
 
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
           <img
-            class="w-[20px] h-[20px]"
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/Notification.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Уведомления</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Уведомления</p>
         </button>
 
-
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
-          <img
-            class="w-[20px] h-[20px]"
+         <img
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/Applications.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Заявки</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Заявки</p>
         </button>
 
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
           <img
-            class="w-[20px] h-[20px]"
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/Favorites.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Избранные</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Избранные</p>
         </button>
 
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
-          <img
-            class="w-[20px] h-[20px]"
+         <img
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/Activity.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Отчеты</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Отчеты</p>
         </button>
 
-
-
         <button
-          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3"
+          class="flex h-[52px] w-[250px] bg-slate-200 items-center text-center px-[24px] gap-3 hover:bg-slate-300"
         >
           <img
-            class="w-[20px] h-[20px]"
+            class="w-[20px] h-[20px] transition-colors duration-300 hover:filter hover:brightness-0 hover:invert-[10%]"
             src="../../public/icons/Setting.svg"
             alt=""
           />
-          <p class="text-lg text-[#475569] font-semibold">Настройки</p>
+          <p
+            class="text-lg text-[#475569] font-semibold transition-colors duration-300 hover:text-[#00B5D7]"
+          >Настройки</p>
         </button>
       </div>
     </div>
