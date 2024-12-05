a2c393368b86df79983ed0f5ef42bf2383abac28
Sobirov S <sobiroff23@gmail.com>
2024-12-05 10:35:46 +0500

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-<div class="flex items-center justify-center">
-  <div class="mt-[64px] h-[88px] w-[80%] bg-[#6E5DC6] rounded-xl">
-        <img class="py-6 pl-[16px]" src="../public/icon puck.svg" alt="">
-  </div>
-</div>
-</template>
-
-<style scoped>
-
-</style>
+    <header>
+      <headerVue />
+    </header>
+  </template>
+  
+  <script>
+  import HeaderVue from "./components/header.vue";
+  
+  export default {
+    name: "App",
+    components: {
+      HeaderVue,
+    },
+  };
+  </script>
+  
+  <style scoped></style>
+  
---
+++ src/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center justify-center">
+      <div class="mt-[64px] h-[88px] w-[80%] bg-[#6E5DC6] rounded-xl">
+        <img class="py-6 pl-[16px]" src="../../public/icon puck.svg" alt="" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HeaderVue",
+  };
+  </script>
+  
+  <style scoped></style>
